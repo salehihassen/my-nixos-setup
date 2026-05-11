7de2a72dc1c6af722f73ab5fe6024e77c6ad7dd5
Saleh Hassen <>
2026-05-11 20:58:28 +0000

    note a few desired todos

--- configuration.nix
+++ configuration.nix
@@ -2,6 +2,9 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
+# TODO: tmux, appimage (helium?), flatpak w/ wayland forced (zen?)
+
+
 { config, lib, pkgs, ... }:
 
 {
