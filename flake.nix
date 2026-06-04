526aecf0f82d8f52a50ac0a5a30eeecfdafd40d1
Saleh Hassen <>
2026-06-04 07:59:35 -0400

    add zen browser repo to my flake

--- flake.nix
+++ flake.nix
@@ -18,6 +18,14 @@
       url = "github:aksiksi/compose2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
+    };
   };
 
   outputs = inputs@{
